9691b784634f10caf031d6d669eed6c72890dfb2
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-04-07 15:44:45 +0200

    fixing error: Warning: You provided a `value` prop to a form field without an `onChange` handler. This will render a read-only field. If the field should be mutable use `defaultValue`. Otherwise, set either `onChange` or `readOnly`

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -4,7 +4,12 @@ const Display = (props) => {
   const { value } = props;
   return (
     <div id="display" className="flex">
-      <input type="text" tabIndex="-1" value={value} />
+      <input
+        type="text"
+        tabIndex="-1"
+        value={value}
+        onChange={(event) => this.setState(event.target.value)}
+      />
     </div>
   );
 };
